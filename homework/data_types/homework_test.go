5ec97bc1e0432b2cf8fa9b508f4fef84918d17d9
study-nv <nvg.study@gmail.com>
2026-05-23 22:09:36 +0300

    Homework data_types lesson

---
+++ homework/data_types/homework_test.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+func ToLittleEndian[T uint16 | uint32 | uint64](number T) T {
+	var res T
+	partsCount := int(unsafe.Sizeof(number))
+
+	for i := 0; i < partsCount; i++ {
+		rp := (*uint8)(unsafe.Add(unsafe.Pointer(&res), partsCount-i-1))
+		p := (*uint8)(unsafe.Add(unsafe.Pointer(&number), i))
+
+		*rp = *p
+	}
+
+	return res
+}
+
+func TestСonversion(t *testing.T) {
+	tests := map[string]struct {
+		number uint32
+		result uint32
+	}{
+		"test case #1": {
+			number: 0x00000000,
+			result: 0x00000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFF,
+			result: 0xFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF,
+			result: 0xFF00FF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF,
+			result: 0xFFFF0000,
+		},
+		"test case #5": {
+			number: 0x01020304,
+			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversion16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #5": {
+			number: 0x0102,
+			result: 0x0201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversion64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF0000FFFF,
+			result: 0xFFFF0000FFFF0000,
+		},
+		"test case #5": {
+			number: 0x0102030405060708,
+			result: 0x0807060504030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
